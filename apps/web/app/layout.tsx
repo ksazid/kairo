e8896084f093a5ada9e00644b939d78008a4fd14
Sazid <47315271+ksazid@users.noreply.github.com>
2026-08-15 20:38:15 +0200

    VS-26: load guided Brand Brain styles

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -7,6 +7,7 @@ import "./discovery.css";
 import "./ideas.css";
 import "./studio.css";
 import "./calendar.css";
+import "./guided-brain.css";
 import { SessionKeepalive } from "./session-keepalive";
 
 export const metadata: Metadata = {
